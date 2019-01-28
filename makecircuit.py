e73eb878295142db4078ac659b3605d3be7ae9b1
mohammad <m.mohammadi9812@gmail.com>
2019-01-28 22:40:12 +0330

    even more logging

--- makecircuit.py
+++ makecircuit.py
@@ -1,20 +1,29 @@
 from MNA.Circuit import *
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s')
 
 def add_element_to_circuit(circuit_list, type, value, neg, pos, out = -1):
     if type == "Resistor":
         circuit_list[0].addResistor(pos, neg, value)
+        logging.debug(f'ressistors: {global_circuit[0].resistors}')
     elif type == 'Battery':
         if str.isdecimal(value):
             value = float(value)
         circuit_list[0].addIndVolt(value, pos, neg)
+        logging.debug(f'battery: {global_circuit[0].indVolt}')
     elif type == 'Current':
         if str.isdecimal(value):
             value = float(value)
         circuit_list[0].addIndCur(pos, neg, value)
+        logging.debug(f'current: {global_circuit[0].independentCurrent}')
     elif type == 'Capacitor':
         circuit_list[0].addCapacitor(value, neg, pos)
+        logging.debug(f'capacitors: {global_circuit[0].capacitors}')
     elif type == 'Inductor':
         circuit_list[0].addInductor(value, neg, pos)
+        logging.debug(f'inductors: {global_circuit[0].inductors}')
     elif type == 'OpAmp':
         circuit_list[0].addOpAmp(neg, pos, out)
+        logging.debug(f'opamps: {global_circuit[0].opAmps}')
 
